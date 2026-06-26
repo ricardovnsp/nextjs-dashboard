25b50db1c7cbe2c9c7d0ae2f73f13615f34d8dcd
Ricardo Valenzuela <RicardoV@natr.com>
2026-06-26 12:52:39 -0600

    first commit my dashboard project

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
